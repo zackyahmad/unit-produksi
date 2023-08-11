ef8328c1bb968246c7b875420d033354e6015e41
ZACKY AHMAD <zackyahmad.zackyahmad307@gmail.com>
2023-08-11 16:32:01 +0700

    add .

---
+++ file.js
@@ -0,0 +1,7 @@
+const navbarNav = document.querySelector('.nav-menu');
+
+// ketka hamburger menu di klik maka
+document.querySelector('#hamburger-menu')
+    onclick = ( ) =>{
+        navbarNav.classList.toggle('active');
+    };
